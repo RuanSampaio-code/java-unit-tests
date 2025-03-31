351fb970551b5fe6737419ba06cd93eca2c7ed72
RuanSampaio-code <ruansampaio001@gmail.com>
2025-03-31 11:49:00 -0300

    testando commit pela interface

--- src/test/java/br/com/dicasdeumdev/api/ApiApplicationTests.java
+++ src/test/java/br/com/dicasdeumdev/api/ApiApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiApplicationTests {
-
+	//teste
 	@Test
 	void main() {
 		ApiApplication.main(new String[] {});
